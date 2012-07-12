92301d784457385d4e1af3eafe958a4a6d908004
saxx <hannes@sachsenhofer.com>
2012-07-12 18:57:36 +0200

    Added AppFail.

--- IssueTracker.Web/Global.asax.cs
+++ IssueTracker.Web/Global.asax.cs
@@ -1,32 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using AppfailReporting.Mvc;
 
-namespace IssueTracker {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
+
+namespace ComicSyndicate.Web {
 
     public class MvcApplication : System.Web.HttpApplication {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new AppfailReportAttribute()); 
+            filters.Add(new HandleErrorAttribute());
+        }
+
+
+        protected void Application_Start() {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<Db, Configuration>());
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            BundleTable.Bundles.EnableDefaultBundles();
+        }
+
+
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "Default",                                              // Route name
-                "{controller}/{action}/{id}",                           // URL with parameters
-                new {
-                    controller = "Home",
-                    action = "Index",
-                    id = ""
-                }  // Parameter defaults
-            );
-
         }
 
-        protected void Application_Start() {
-            RegisterRoutes(RouteTable.Routes);
-        }
     }
 }
