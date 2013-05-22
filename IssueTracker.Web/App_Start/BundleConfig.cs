403cabd29f8d7aec7e14e5c271e72cd445f8c218
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2013-05-22 15:16:18 +0200

    Updated to jQuery 2.0.

--- IssueTracker.Web/App_Start/BundleConfig.cs
+++ IssueTracker.Web/App_Start/BundleConfig.cs
@@ -4,13 +4,12 @@ namespace CloudPass.Web {
     public class BundleConfig {
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/js/jquery").Include(
-                "~/Scripts/jquery-1.8.0.js"));
+                "~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
                 "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/css/bootstrap").Include(
                 "~/Content/bootstrap.css"));
-
             bundles.Add(new StyleBundle("~/css/bootstrap-responsive").Include(
                 "~/Content/bootstrap-responsive.css"));
         }
