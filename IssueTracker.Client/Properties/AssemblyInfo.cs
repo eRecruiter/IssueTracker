3f0ec62f85da339994aeac4bade76d8b882b091c
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2014-01-22 16:50:20 +0100

    Bumped the version of the NuGet package to 1.0

--- IssueTracker.Client/Properties/AssemblyInfo.cs
+++ IssueTracker.Client/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
