1966c92f31edc8d1454737c126c1b432bdd51cfc
Hannes Sachsenhofer <hannes@sachsenhofer.com>
2014-01-29 16:59:13 +0100

    Added DEL keyboard shortcut.

--- ePunkt.IssueTracker/App_Start/BundleConfig.cs
+++ ePunkt.IssueTracker/App_Start/BundleConfig.cs
@@ -7,7 +7,7 @@ namespace ePunkt.IssueTracker.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/js").Include(
-                "~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js"));
+                "~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js", "~/Scripts/mousetrap-{version}.js", "~/Scripts/mousetrap-global-bind.js"));
 
             bundles.Add(new StyleBundle("~/css/css").Include(
                 "~/Content/bootstrap.css", "~/Content/site.css"));
